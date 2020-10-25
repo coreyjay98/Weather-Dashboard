a48a2ac1b1fd61633c5915dfca19e93bbd79d8c1
Corey Samuels <coreyjsamuels@gmail.com>
2020-10-25 23:01:24 +0000

    commiting initial files

---
+++ ReadMe.md
@@ -0,0 +1,21 @@
+# Weather Dashboard
+
+## Brief
+
+This project I was assigned the task of creating a wather dashboard that used ajax calls from API's to gater information about weather and display that to the user when they searched for their city of choice. I had to create a simplistic UI that was easy to use and provide correct and accurate information about the weather for the user.
+
+### Review
+
+For me this project started out easier than many of the projects I have completed before as I felt like I have acquired a good understanding of ajax and so being able to navigate through the data felt quite comfortable to me. I began to struggle once I had created the display and needed to save and recall the search history. To combat my problem I used arrays to store the previous search and was able to create a button that displayed the previous search if the user needed to revisit. This proved to be a successful progression for me as I ran into a problem as was able to overcome it through the power of Java!
+
+Access the dashboard at
+
+### Credits
+
+- Jquery
+
+- Bootstrap
+
+- Weather API
+
+- Font Awesome
---
+++ script.js
@@ -0,0 +1,186 @@
+const searchForm = $(".searchForm");
+const citySearch = $(".citySearch");
+const UVindexList = $(".UVindexList");
+/* let citySearchStorage = sessionStorage.getItem("city name");
+console.log("city name", citySearchStorage); */
+/* if (citySearchStorage === "") {
+  citySearch.val(citySearchStorage);
+} */
+
+const historyArray = [];
+
+function onSearchClick() {
+  clearDisplay();
+  const cityQuery = citySearch.val();
+  const currentQueryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityQuery +
+    "&appid=39396cbe65ca20ac56da20de7be724d3";
+  const fiveDayURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityQuery +
+    "&appid=39396cbe65ca20ac56da20de7be724d3";
+  sessionStorage.setItem("city name", cityQuery);
+
+  $.ajax({
+    url: currentQueryURL,
+  })
+    .then(function (response) {
+      UVIndexRetrieve(response);
+      currentDisplay(response);
+      console.log("response", response);
+    })
+    .catch(function () {
+      alert("Please Enter a Valid City");
+    });
+
+  $.ajax({
+    url: fiveDayURL,
+  })
+    .then(function (fiveDay) {
+      fiveDayDisplay(fiveDay);
+    })
+    .catch(function () {
+      console.log("Not a valid city");
+    });
+
+  function UVIndexRetrieve(response) {
+    const cityLat = response.coord.lat.toString();
+    const cityLon = response.coord.lon.toString();
+    const UVindexURL =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      cityLat +
+      "&lon=" +
+      cityLon +
+      "&appid=39396cbe65ca20ac56da20de7be724d3";
+
+    $.ajax({
+      url: UVindexURL,
+    }).then(function (UVindex) {
+      UVindexDisplay(UVindex);
+    });
+  }
+  $(".citySearch").val("");
+  historyArray.push(cityQuery);
+}
+searchForm.submit(function (event) {
+  event.preventDefault();
+  onSearchClick();
+  historyButtons();
+});
+
+function UVindexDisplay(UVindex) {
+  const UVindexVal = UVindex.value;
+  UVindexList.append("UV Index : " + UVindexVal);
+  if (UVindexVal < 3) {
+    UVindexList.addClass("greenUV");
+  } else if (UVindexVal > 3 && UVindexVal < 6) {
+    UVindexList.addClass("orangeUV").removeClass("greenUV");
+  } else {
+    UVindexList.addClass("redUV").removeClass("greenUV orangeUV");
+  }
+}
+
+function currentDisplay(response) {
+  $(".currentHead").text("Current Weather In " + response.name);
+  let currentResults =
+    "<li class='temperatureList'>" +
+    "Temperature : " +
+    Math.floor(response.main.temp - 273.15) +
+    " °C";
+  currentResults +=
+    "<li class='humidityList'>" +
+    "Humidity : " +
+    response.main.humidity +
+    "%" +
+    "</li>";
+  currentResults +=
+    "<li class='windSpeedList'>" +
+    "Wind Speed : " +
+    response.wind.speed +
+    " MPH";
+  +"</li>";
+  $(".displayList").append(currentResults);
+}
+
+function historyButtons() {
+  for (let selector = 0; selector < historyArray.length; selector++) {
+    let searchHistoryButton =
+      "<button class='searchHistory'>" + historyArray[selector] + "</button>";
+    $(".historyContainer").append(searchHistoryButton);
+    console.log("button");
+  }
+}
+
+function fiveDayDisplay(fiveDay) {
+  const cardInfo = fiveDay.list;
+  $(".fiveForecast").append(
+    "<h2 class='fiveHeader'> " + "Five Day Forecast" + "</h2>"
+  );
+  for (let selector = 0; selector < cardInfo.length; selector++) {
+    if (cardInfo[selector].dt_txt.includes("12:00:00")) {
+      let cardDate = cardInfo[selector].dt_txt;
+      let cardTemp = cardInfo[selector].main.temp;
+      let cardHumidity = cardInfo[selector].main.humidity;
+      let cardCreate =
+        "<div class='coreyCard'>" +
+        "<h6 class='cardDate'>" +
+        cardDate.slice(0, 10) +
+        "</h6>";
+      cardCreate +=
+        "<h6 class = 'cardTemp'>" +
+        "Temp:" +
+        Math.floor(cardTemp - 273.15) +
+        " °C " +
+        "</h6>";
+      cardCreate +=
+        "<h6 class=' cardHumidity'>" +
+        "Humidity:" +
+        cardHumidity +
+        "%" +
+        "</h6>" +
+        "</div>";
+      $(".fiveForecast").append(cardCreate);
+      localStorage.setItem("firstChoice", cardCreate);
+    }
+  }
+}
+
+const searchHistorySubmission = $(".searchHistory");
+
+$(".historyContainer").on("click", function () {
+  clearDisplayButton(event);
+  const lastItem = historyArray[historyArray.length - 1];
+  if ($(".searchHistory").text().includes(lastItem)) {
+    console.log("first array", historyArray);
+    console.log(lastItem);
+    $(".citySearch").val(lastItem);
+    onSearchClick();
+  } else {
+    console.log("second array", historyArray);
+    $(".citySearch").val(lastItem);
+    console.log(lastItem);
+    onSearchClick();
+    historyButtons();
+  }
+});
+
+function clearDisplay(event) {
+  $("ul").text("");
+  $(".fiveForecast").empty();
+  $(".UVindexList").empty();
+}
+function clearDisplayButton(event) {
+  $("ul").text("");
+  $(".fiveForecast").empty();
+  $(".UVindexList").empty();
+  $(".citySearch").val("");
+}
+/* $(".citySearch").val($(".searchHistory").text().trim());
+console.log($(".citySearch").val()); */
+
+/* if ($(this).hasClass("searchHistory")) {
+  console.log($(".citySearch").val($(this).text().trim()));
+  UVindexDisplay();
+} else {
+} */
